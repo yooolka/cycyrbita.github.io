34f2457f96c72e8447b84f29db8a8a8b24819317
Громов Максим Борисович <m.gromov@leadtop.local>
2020-09-04 17:20:31 +0300

    добавил чистый ленд

---
+++ clear lends/co9.cleanvisionnd.com/js/translater.js
@@ -0,0 +1,174 @@
+var new_lang = {
+
+"lt1": "Recupera <span>la visión</span> <i> <mark>en 1 curso</mark> </i>",
+
+"lt2": " <span>Alivia la tensión</span> y el dolor en los ojos en 5 minutos",
+
+"lt3": " <span>Recupera la visión</span> al 99% en 1 curso",
+
+"lt4": " <span>Previene</span> enfermedades oculares y disminución de la visión",
+
+"lt5": " <b>Afecta</b> <span>la causa</span> de la mala visión",
+
+"lt6": " <b>Protege</b> <span>en cargas</span> prolongadas",
+
+"lt7": " <b>Alivia</b>la tensión",
+
+"lt8": "Pide <span>con descuento</span>  <span> <mark>ahora mismo</mark> </span> ",
+
+"lt11": "La oferta acaba en:",
+
+"lt12": "<div class=\"form__count-item\"> <span>0</span>  <span>horas</span> </div>:<div class=\"form__count-item\"> <span>11</span>  <span>minutos</span> </div>:<div class=\"form__count-item\"> <span>56</span>  <span>segundos</span> </div>",
+
+"lt13": "País",
+
+"lt14": "Tu nombre",
+
+"lt15": "Teléfono",
+
+"lt16": "Comprar con descuento",
+
+"lt17": "¿Por qué <mark>la visión se deteriora?</mark> ",
+
+"lt18": "La agudeza visual depende de la lente y los músculos oculares. Cuando los músculos se debilitan y la elasticidad de la lente disminuye, la visión baja rápidamente.",
+
+"lt19": "Esto sucede por varias razones:",
+
+"lt20": "Cambios de edad",
+
+"lt21": "Procesos Inflamatorios y enfermedades oculares",
+
+"lt22": "Aumento de la presión en el globo ocular",
+
+"lt23": "Aumento de la carga ocular",
+
+"lt24": "Suministro Insuficiente de sangre",
+
+"lt25": "<mark>En ausencia de tratamiento, la visión se reduce gradualmente a ceguera total</mark> . Las gafas y las lentes de contacto solo empeoran la situación: los músculos oculares dejan de funcionar por completo, esto hace que la visión disminuya aún más rápido.",
+
+"lt26": "El aparato muscular enfoca los ojos",
+
+"lt27": "para que las imágenes resultantes, al pasar a través de la lente, se proyecten en la retina",
+
+"lt28": "Trastornos en el equilibrio muscular",
+
+"lt29": "crean una compresión insuficiente o excesiva del globo ocular",
+
+"lt30": "La lente se deforma",
+
+"lt31": "enfoque distorsiona, imagen se nubla",
+
+"lt32": "¡Puedes salvar <span>tu visión!</span> ",
+
+"lt33": "Los científicos del Centro europeo de oftalmología pasaron 8 años creando el medicamento XX. El medicamento ha pasado pruebas de varios pasos, ha obtenido la licencia y los certificados de calidad, así como una serie de premios para el mejor desarrollo en oftalmología.",
+
+"lt34": "La característica peculiar de este remedio novedoso es el efecto más rápido posible, notable ya después de 1 curso.",
+
+"lt35": "Recomendado para uso <mark>por la asociación de oftalmólogos de la UE</mark> ",
+
+"lt36": "100% <mark>composición</mark> natural",
+
+"lt37": "Eficacia <mark>probada clínicamente</mark> ",
+
+"lt39": " <mark>Desarrollo innovador</mark> para recuperar la visión",
+
+"lt40": "XX es un complejo biogénico de extractos naturales que tienen un efecto directo en caso de la disminución de visión y proporcionan una eliminación completa de las enfermedades oculares más comunes de la persona.",
+
+"lt41": "Es el único remedio que regula el funcionamiento de los músculos del globo ocular, lo que ayuda a recuperar la focalización normal y devolver una visión clara, nítida y volumétrica",
+
+"lt42": "La composición de XX incluye <span> <mark>ingredientes de alta calidad </mark> </span> con eficacia probada",
+
+"lt43": "EXTRACTO DE ARÁNDANOS",
+
+"lt44": "Mejora la agudeza visual, fortalece la retina ocular",
+
+"lt45": "UVA DE PLAYA Y RAUVOLFIA",
+
+"lt46": "Protegen la retina de las lesiones fotográficas. Mejoran la agudeza visual",
+
+"lt47": "EXTRACTO DE GINKGO BILOBA",
+
+"lt48": "Mejora la microcirculación Previene la fragilidad capilar. Mejora el flujo sanguíneo en los vasos de la retina del ojo.",
+
+"lt49": "ACEITE DE TIBURÓN",
+
+"lt50": "Mejora la elasticidad vascular, la agudeza visual y el contraste de la percepción de la imagen. Normaliza la presión intraocular, previene glaucoma",
+
+"lt51": "EMPETRUM",
+
+"lt52": "Previene la disminución de la visión asociada con la edad. Reduce el riesgo de degeneración macular. Ayuda en la prevención de las cataratas",
+
+"lt53": "VITAMINAS B, C, E",
+
+"lt54": "Mejoran el estado de los órganos de la visión. Participan en los procesos de adaptación oscura, reducen la fatiga ocular. Aceleran la recuperación de pigmentos visuales. Ayudan a prevenir las cataratas.",
+
+"lt55": "Opinión del<mark>especialista</mark> ",
+
+"lt56": "En los últimos años el número de personas que sufren de pérdida de visión ha aumentado 18 veces. Muchos de mis pacientes entran en pánico, me piden que les recete lentes y gafas, aunque no se dan cuenta de que tal medida en el futuro empeorará aún más la situación al interrumpir por completo el trabajo de los ojos.",
+
+"lt57": "Hoy existe una oportunidad de RESOLVER el problema. Para estos fines, crearon el medicamento XX. Se lo prescribo a cada paciente y en el 99% de los casos logran devolver una visión clara y nítida después de tomar 1 curso.",
+
+"lt58": "La composición del remedio incluye componentes especialmente seleccionados que recuperan el correcto funcionamiento de los músculos oculares y mejoran el estado del cristalino, que son los principales factores de buena visión.",
+
+"lt59": " <mark>Carlos Mora</mark> , doctor en medicina, <span>oftalmólogo</span> con experiencia de 20 años",
+
+"lt60": "Opiniones",
+
+"lt61": "Claudia Sosa,",
+
+"lt62": "35 años",
+
+"lt63": "De XX me enteré en la clínica, allí lo discutían activamente, así que decidí pedirlo para probar, por qué no? La edad ya me molestaba un poco, a menudo tenía que llevar las gafas, porque por ejemplo no veía el número de autobús. El oftalmólogo me hacía unas previsiones decepcionantes: me comenzaron los problemas de retina. Y se dice que XX se toma con todos los problemas con los ojos. Bueno, ¡es maravilloso! Ahora veo todo mucho mejor, incluso los pequeños detalles en una distancia. ¡Así que lo recomiendo con seguridad!",
+
+"lt64": "Belén Morales,",
+
+"lt65": "29 años",
+
+"lt66": "Después del parto, comenzaron los problemas de visión. Al parecer, la presión me saltó y la visión empeoró drásticamente. Me dijeron que debía pasar al cabo de un tiempo. Pero después de un año solo empeoró. Tenía dolores, rojez, sequedad. De -0.5 se ha convertido en -2. Fui a ver a un médico pagado porque en la clínica sólo decían que iba a pasar. Un médico normal en una clínica pagada me recetó XX ¡y las cosas se pusieron bien en un mes! La visión vuelve gradualmente, ya no es -2, sino sólo -1, casi 100% de la visión. ¡Me olvidé hace mucho tiempo de la presión y el dolor, y mucho menos de la sequedad!",
+
+"lt67": "Felipe Alavrez",
+
+"lt68": "42 años",
+
+"lt69": "Soy programador y el hecho de trabajar frente a la pantalla ha afectado mi visión mucho. Llegué al punto en que me levantaba de la computadora, y todo flotaba ante mis ojos. Dolores de cabeza constantes debido a la tensión en los ojos. Dejé de trabajar cuando los ojos dejaron de distinguir las letras pequeñas en la pantalla. Tomé unas vacaciones, y me pedí XX. Dos semanas di a mis ojos un descanso, tomé el medicamento y durante 15 minutos al día hacía gimnasia. El resultado me sorprendió. Veo todo tan claro como si me hubieran limpiado los ojos! Ahora siempre me cuidaré de la visión!",
+
+"lt71": "Recupera <span>la visión</span> <i> <mark>en 1 curso</mark> </i>",
+
+"lt72": " <span>Alivia la tensión</span> y el dolor en los ojos en 5 minutos",
+
+"lt73": " <span>Recupera la visión</span> al 99% en 1 curso",
+
+"lt74": " <span>Previene</span> enfermedades oculares y disminución de la visión",
+
+"lt75": " <b>Afecta</b> <span>la causa</span> de la mala visión",
+
+"lt76": " <b>Protege</b> <span>en cargas</span> prolongadas",
+
+"lt77": " <b>Alivia</b>la tensión",
+
+"lt78": "Pide <span>con descuento</span>  <span> <mark>ahora mismo</mark> </span> ",
+
+"lt81": "La oferta acaba en:",
+
+"lt82": "<div class=\"form__count-item\"> <span>0</span>  <span>horas</span> </div>:<div class=\"form__count-item\"> <span>11</span>  <span>minutos</span> </div>:<div class=\"form__count-item\"> <span>56</span>  <span>segundos</span> </div>",
+
+"lt83": "País",
+
+"lt84": "Tu nombre",
+
+"lt85": "Teléfono",
+
+"lt86": "Comprar con descuento"
+
+
+};
+function Translater () {
+ for (class_name in new_lang) {
+ var elements = document.getElementsByClassName(class_name);
+ if (elements.length) {
+ for (key in elements) {
+ elements[key].innerHTML = new_lang[class_name];
+ }
+ }
+ }
+ };
