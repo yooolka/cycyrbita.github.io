cd3d08cc7bdfa3c165caf1f2bed9aaeceb98540b
Громов Максим Борисович <m.gromov@leadtop.local>
2020-09-09 14:41:24 +0300

    2_osteowell_prelend добавил форму

--- product/osteowell/prelends/2_osteowell_prelend/js/index.js
+++ product/osteowell/prelends/2_osteowell_prelend/js/index.js
@@ -151,3 +151,35 @@ window.onload = function() {
         };
     };
 };
+
+/*таймер*/
+(function (min, sec) {
+    var date = new Date(),
+        count = 0;
+    date.setMinutes(15); /*ставим необходимое время*/
+    date.setSeconds(0); /*ставим необходимое время*/
+    min[0].textContent = zero(date.getMinutes());
+    sec[0].textContent = zero(date.getSeconds());
+
+    var interval = setInterval(counter, 1000);
+
+    function counter() {
+        count++;
+        var liveDate = new Date();
+        liveDate.setMinutes(date.getMinutes());
+        liveDate.setSeconds(date.getSeconds() - count);
+
+        min[0].textContent = zero(liveDate.getMinutes());
+        sec[0].textContent = zero(liveDate.getSeconds());
+        if (min[0].textContent === '00' && sec[0].textContent === '00') {
+            clearInterval(interval);
+        }
+    }
+
+    function zero(num) {
+        return num.toString().length === 1 ? '0' + num : num;
+    }
+})(
+    document.getElementsByClassName('min'),/*присваиваем классы*/
+    document.getElementsByClassName('sec')/*присваиваем классы*/
+)
